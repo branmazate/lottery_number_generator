15bb4ff45767f3881e6afec438dcc940e938d9c4
Fermazate <fernandomazate@gmail.com>
2024-09-10 13:27:17 -0600

    FIXED (01-BM-EDA.ipynb): The regular expression in the variable other_class did not correctly represent what was being collected. It collected the next number as part of the prize for the previous number.

--- src/utils/web_scraping.py
+++ src/utils/web_scraping.py
@@ -12,7 +12,8 @@ class url_scraper:
         url (str): The URL to scrape.
         """
         self.url = ''
-        self.html_content = ""
+        self.main_html_content = ""
+        self.secondary_html_content = ""
         self.data = []
         self.response = requests.get(url)
         self.soup = BeautifulSoup(self.response.content, 'html.parser')
@@ -50,51 +51,6 @@ class url_scraper:
         except Exception as e:
             print(f'Error in extracting data {e}')
                 
-
-    # def scrape_data(self, tag:str, main_class:str, other_class:str)->str:
-    #     """Scrapes html raw text from the given tag, main_class, and other_class.
-
-    #     Args:
-    #         tag (str): _description_
-    #         main_class (str): _description_
-    #         other_class (str): _description_
-
-    #     Returns:
-    #         str: _description_
-    #     """
-    #     try:
-    #         #Extracting main prizes
-    #         main_tag = self.soup.find(tag, class_ = main_class)
-    #         if main_class:
-    #             main_class_text = main_tag.get_text(separator='|').strip()
-    #             print("Main Prize Div Content:",main_prizes_text)
-    #             main_prizes_pattern = r'PRIMER PREMIO(\d{5}) \|\|\| SEGUNDO PREMIO (\d{5}) \|\|\| TERCER PREMIO (\d{5})'
-    #             main_prizes_match = re.findall(main_prizes_pattern, main_prizes_text)
-                
-    #             if main_prizes_match:
-    #                 main_prizes = main_prizes_match[0]
-    #                 print(f'Main Prizes: {main_prizes}')
-    #             else:
-    #                 print('Not enough main prizes found.')
-    #                 main_prizes = None
-    #         else:
-    #             print('Main prize div not found.')
-    #         #Extracting other prizes
-    #         other_prizes_div = self.soup.find('div',class_=other_prize_classes)
-    #         if other_prizes_div:
-    #             other_prizes_text = other_prizes_div.get_text(' ').strip()
-    #             print(f"Other prize div content: {other_prizes_text}")
-                
-    #             #Regular expression to extract the prizes
-    #             other_prizes_pattern = r'(\d{5})\s+([A-Z]{1,3})\s+\.{4}\s+([\d.,]+)'
-    #             other_prizes = re.findall(other_prizes_pattern, other_prizes_text)
-    #             print(f'Other Prizes: {other_prizes}')
-    #         else:
-    #             print('Other prize div not found.')
-    #             other_prizes = None
-    #         return main_prizes, other_prizes
-    #     except Exception as e:
-    #         print(f'Error in extracting data: {e}')
         
     # # Extract winner prizes from the HTML
     # def extract_winner_numbers(self, start_id, end_id):
