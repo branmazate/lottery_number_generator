940869a86c72b9346171acb30507b8132b955eb6
Fermazate <fernandomazate@gmail.com>
2024-09-18 14:21:41 -0600

    ADD: Turnt to csv function

--- src/utils/web_scraping.py
+++ src/utils/web_scraping.py
@@ -14,20 +14,21 @@ class url_scraper:
         self.url = ''
         self.main_html_content = ""
         self.secondary_html_content = ""
-        self.data = []
+        self.other_data = []
+        self.main_data = []
         
-    def scrape_data(self, url:str, tag:str, main_class:str, data:dict, other_class:str = None, main_class_pattern:str=None, other_class_pattern:str=None):
+    def scrape_data(self, url:str, tag:str, main_class:str, other_class:str = None, main_class_pattern:str=None, other_class_pattern:str=None):
         "Extracts de text from the given tag, main_class, and other_class based on a regular expression pattern."
         #TODO Add error handling for the case when the tag is not found.
         #TODO Add error handling for the case when the main_class is not found.
         #TODO Add error handling for the case when the other_class is not found.
         #TODO Add html content to the data obtained
+        #FIXME All the entries in the main and other data are the same.
         try:
             #Extracting main class data.
             self.url = url
             self.response = requests.get(self.url)
             self.soup = BeautifulSoup(self.response.content, 'html.parser')
-            self.data = data
             tag_content = self.soup.find(tag, class_=main_class) #Gets the info from the main class
             if tag_content:
                 main_class_text = tag_content.get_text().strip()
@@ -36,6 +37,7 @@ class url_scraper:
                     main_data = re.findall(main_class_pattern, main_class_text)
                     if main_data:
                         print(f'Extracted main data based on pattern: {main_data}')
+                        self.main_data.append(main_data)
                     else:
                         print(f'No main data found based on pattern: {main_class_pattern}')
             #Extracting other class data.
@@ -48,6 +50,7 @@ class url_scraper:
                         other_data = re.findall(other_class_pattern, other_class_text)
                         if other_data:
                             print(f'Extracted other data based on pattern: {other_data}')
+                            self.other_data.append(other_data)
                         else:
                             print(f'No other data found based on pattern: {other_class_pattern}')
             else:
@@ -62,20 +65,29 @@ class url_scraper:
             url = f'{url}{url_pattern}{id}'
             self.scrape_data(url,tag=tag,main_class=main_class,other_class=other_class,main_class_pattern=main_class_pattern,other_class_pattern=other_class_pattern)
     
-      
-    # def save_to_csv(self, filepath='scraped_data.csv'):
-    #     """
-    #     Saves the scraped data to a csv file
-        
-    #     Parameters:
-    #     filename(str): The name of the CSV file to save the data
-    #     """
-        
-    #     try:
-    #         #Convert the data to a DataFrame
-    #         df = pd.DataFrame(self.data)
-    #         #Save DataFrame to a CSV ile
-    #         df.to_csv(filepath, index=False)
-    #         print(f'Data succesfully saved to {filepath}')
-    #     except Exception as e:
-    #         print(f'Error saving data to CSV: {e}')
+    def turn_to_df(self, main_dict, other_dict=None,filepath = None):
+        try:
+            #Convert the data to a DataFrame
+            for text in self.main_data:
+                main_dict[0] = text[0]
+                main_dict[1] = text[1]
+                main_dict[2] = text[2]
+            if other_dict:
+                for text in self.other_data:
+                    other_dict[0] = text[0]
+                    other_dict[1] = text[1]
+                    other_dict[2] = text[2]
+            main_df = pd.DataFrame(main_dict)
+            other_df = pd.DataFrame(other_dict)
+            
+            df = pd.concat([main_df, other_df], ignore_index=True)
+            
+            if filepath:
+                df.to_csv(filepath, index=False)
+                print(f'Data succesfully saved to {filepath}')
+            else:
+                print(f'Data succesfully saved')
+                return df
+            
+        except Exception as e:
+            print(f'Error saving data to CSV: {e}')
